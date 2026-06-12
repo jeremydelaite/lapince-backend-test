843c72a2deb5200aadebe8fb39e1ec4f4d273e2f
jeremydelaite <jeremy.delaite@oclock.school>
2026-06-12 16:24:24 +0200

    fix/error2

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,4 +1,7 @@
 export const allowedOrigins =
 	process.env.NODE_ENV === "production"
-		? ["https://lapince-frontend.vercel.app", "https://lapince-jeremydelaites-projects.vercel.app"]
+		? [
+				"https://lapince-frontend.vercel.app",
+				"https://lapince-jeremydelaites-projects.vercel.app",
+			]
 		: ["http://localhost:5173"];
